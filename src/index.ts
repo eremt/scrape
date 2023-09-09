49a26854262e402226822ebf77d085e6496e806f
Troy <eremtroy@gmail.com>
2023-09-09 11:21:09 +0200

    feat: implement scrape and parse with some tests

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,60 @@
-export default function () {
-  return true
+import { load } from 'cheerio'
+
+type Schema = { [key: string]: string | SchemaList | SchemaFunction }
+type SchemaFunction = ($: any, context: any) => any
+type SchemaList = any[]
+
+export function parseText ($: any, selector: string, context?: any) {
+  return $(selector, context).text().trim()
+}
+
+export function parse (schema: Schema = {}, $: any, context?: any) {
+  const entries = Object.entries(schema)
+  const result: { [key:string]: string } = entries.reduce((obj: any, [key, value]) => {
+    let type: string = typeof value
+    if (Array.isArray(value)) type = 'array'
+
+    switch (type) {
+      case 'string':
+        obj[key] = parseText($, value as string, context)
+        break
+
+      case 'array':
+        const [ listSchema, listContext ] = value as SchemaList
+        if (!listSchema) {
+          throw new Error('Selector is required')
+        }
+        if (typeof listSchema === 'string') {
+          obj[key] = $(listSchema, context).map((_: any, el: any) => parseText($, el)).get()
+        } else {
+          if (!listContext) {
+            throw new Error('ListSelector with SchemaSelector requires context')
+          }
+          const matches = $(listContext, context)
+          obj[key] = matches.map((_: any, el: any) => {
+            const result = parse(listSchema, $, el)
+            return result
+          }).get()
+        }
+        break
+
+      case 'function':
+        const cb = value as SchemaFunction
+        obj[key] = cb($, context)
+        break
+    }
+    return obj
+  }, {})
+  return result
+}
+
+export default function scrape (html: string | Buffer, schema: Schema) {
+  if (!html) throw new Error('parse() requires argument html')
+  if (!schema) throw new Error('parse() requires argument schema')
+  try {
+    const cheerioDocument = load(html)
+    return parse(schema, cheerioDocument)
+  } catch (error)  {
+    console.error(error)
+  }
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,119 @@
-import project from '../src/index'
+import scrape, { parse }  from '../src/index'
+import * as cheerio from 'cheerio'
 
-describe('test', () => {
-  it('runs', () => {
-    expect(project()).toBe(true)
+const html = `<!doctype html>
+<head>
+  <title>Site title</title>
+</head>
+<body>
+  <main>
+    <div class="item item-1">
+      <h1>Title of item-1</h1>
+      <p class="lead">Lead paragraph of item-1</p>
+      <p>Second paragraph of item-1</p>
+      <p>Third paragraph of item-1</p>
+    </div>
+    <div class="item item-2">
+      <h1>Title of item-2</h1>
+      <p class="lead">Lead paragraph of item-2</p>
+      <p>Second paragraph of item-2</p>
+      <p>Third paragraph of item-2</p>
+    </div>
+    <div class="item item-3">
+      <h1>Title of item-3</h1>
+      <p class="lead">Lead paragraph of item-3</p>
+      <p>Second paragraph of item-3</p>
+      <p>Third paragraph of item-3</p>
+    </div>
+  </main>
+</body>`
+
+const htmlCheerio = cheerio.load(html)
+
+describe('scrape', () => {
+  beforeEach(() => {
+    jest.restoreAllMocks()
+  })
+
+  it('throws without html', () => {
+    // @ts-ignore
+    expect(() => scrape(undefined, undefined)).toThrow()
+  })
+
+  it('throws with html without schema', () => {
+    // @ts-ignore
+    expect(() => scrape(html, undefined)).toThrow()
+  })
+
+  it('loads html with cheerio', () => {
+    const load = jest.spyOn(cheerio, 'load')
+    scrape(html, {})
+    expect(load).toBeCalledWith(html)
+  })
+})
+
+describe('parse', () => {
+  let $: any
+  $ = jest.spyOn(cheerio, 'load')
+  $.mockImplementation((sel: any, ctx: any) => htmlCheerio(sel, ctx))
+  beforeEach(() => {
+    jest.restoreAllMocks()
+  })
+
+  it('parses string selector', () => {
+    const result = parse({ title: 'title' }, $)
+    expect(result).toEqual({ title: 'Site title' })
+  })
+
+  it('throws array without selector', () => {
+    expect(() => parse({ headings: [] }, $)).toThrow()
+  })
+
+  it('parses array string selector', () => {
+    const headings = [
+      'Title of item-1',
+      'Title of item-2',
+      'Title of item-3',
+    ]
+    const result = parse({ headings: ['h1'] }, $)
+    expect(result).toEqual({ headings })
+  })
+
+  it('throws array schema selector without context', () => {
+    expect(() => parse({ headings: [ { heading: 'h1' } ] }, $)).toThrow()
+  })
+
+  it('parses array schema selector with context', () => {
+    const items = [
+      { heading: 'Title of item-1', lead: 'Lead paragraph of item-1' },
+      { heading: 'Title of item-2', lead: 'Lead paragraph of item-2' },
+      { heading: 'Title of item-3', lead: 'Lead paragraph of item-3' },
+    ]
+
+    const listSchema = { heading: 'h1', lead: 'p.lead' }
+
+    const resultAll = parse({ items: [ listSchema, '.item' ] }, $)
+    expect(resultAll).toEqual({ items })
+
+    const resultOne = parse({ items: [ listSchema, '.item-1' ] }, $)
+    expect(resultOne).toEqual({ items: [ items[0] ] })
+  })
+
+  it('parses function selector', () => {
+    const headings = [
+      'Title of replaced-1',
+      'Title of replaced-2',
+      'Title of replaced-3',
+    ]
+    function fn (_$: any) {
+      const tags = _$('h1')
+      const replaced = tags.map((_: any, el: any) => {
+        const text = _$(el).text().trim()
+        return text.replace('item', 'replaced')
+      }).get()
+      return replaced
+    }
+    const result = parse({ headings: fn }, $)
+    expect(result).toEqual({ headings })
   })
 })
